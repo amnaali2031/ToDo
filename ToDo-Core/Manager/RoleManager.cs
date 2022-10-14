e8dd6c3b2a38d871e603c8e8f546c3da6a4b7fbe
hp <hp@DESKTOP-R1REQ4G>
2022-10-14 03:10:17 +0300

    Add .gitattributes, .gitignore, and README.md.

---
+++ ToDo-Core/Manager/RoleManager.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System.Linq;
+using ToDo_Core.Manager.Interface;
+using ToDo_MainProject.Model;
+using ToDo_ModelView.ModelView;
+
+namespace ToDo_Core.Manager
+{
+    public class RoleManager : IRoleManager
+    {
+        
+             private DB_ToDoContext _DB;
+        private IMapper _mapper;
+
+        public RoleManager(DB_ToDoContext db, IMapper mapper)
+        {
+            _DB = db;
+            _mapper = mapper;
+        }
+
+        public bool CheckAccess(UserModel userModel)
+        {
+            var isAdmin = _DB.Users
+                                   .Any(a => a.Id == userModel.Id
+                                             && a.IsAdmin==1);
+            return isAdmin;
+        }
+
+    
+    }
+}
---
+++ ToDo-Core/Mapper/Mapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo_MainProject.Model;
+using ToDo_ModelView.ModelView;
+
+namespace ToDo_Core.Mapper
+{
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<User, LoginUserResponse>().ReverseMap();
+            CreateMap<UserModel, User>().ReverseMap();
+
+
+        }
+    }
+}
